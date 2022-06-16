83fd58301e191f2d4659fe6c2342539a69d79fe6
Milton Chandradas <milton.chandradas@live.com>
2022-06-16 16:35:30 -0400

    tenant specific endpoint...

--- scheduler/server.js
+++ scheduler/server.js
@@ -1,46 +1,190 @@
+const https = require("https");
+const axios = require("axios");
 const express = require("express");
 const app = express();
 app.use(express.json());
 
 const VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES);
-const JOB_CREDENTIALS = VCAP_SERVICES.jobscheduler[0].credentials;
+const JOBSCH_CREDENTIALS = VCAP_SERVICES.jobscheduler[0].credentials;
+const JOBSCH_CLIENTID = JOBSCH_CREDENTIALS.uaa.clientid;
+const JOBSCH_SECRET = JOBSCH_CREDENTIALS.uaa.clientsecret;
+const JOBSCH_URL = JOBSCH_CREDENTIALS.url; //"https://jobscheduler-rest.cfapps.eu10-004.hana.ondemand.com"
 
-/* Application Server */
+/* App server */
+app.listen(process.env.PORT, () => {});
 
-app.listen(process.env.PORT, function () {
-   console.log("===> Server started");
+/* App endpoints */
+
+app.get("/app", function (req, res) {
+   const url = `https://${req.hostname}/createjob`;
+   res.send(
+      `<h1>Homepage</h1><h4>Products List</h4><p>...</p>Click <a href="${url}">here</a> to update your status.`
+   );
 });
 
-/* Application endpoints */
+app.get("/createjob", async function (req, res) {
+   const hostname = req.hostname;
+   const subdomain = hostname.substring(0, hostname.indexOf("-"));
+   const jwtToken = await fetchJWTToken(subdomain);
+   const result = await createJob(jwtToken, subdomain);
 
-app.get("/app", (req, res) => {
-   res.send(`<h1>Homepage of multitenant app</h1>`);
+   res.send(
+      `Job created for customer ${subdomain}. Check dashboard/CF logs. Result of job creation: ${JSON.stringify(
+         result
+      )}`
+   );
 });
 
-app.get("/action", (req, res) => {
-   res.send(`ACTION endpoint for jobscheduler successfully invoked.`);
+app.get("/action", function (req, res) {
+   res.send(
+      `"/action" endpoint invoked by jobscheduler. Customer status updated for ${req.hostname}.`
+   );
 });
 
 /* Multi Tenancy callbacks */
 
 app.get("/handleDependencies", (req, res) => {
-   const dependencies = [{ xsappname: JOB_CREDENTIALS.uaa.xsappname }];
-   res.status(200).json(dependencies);
+   res.status(200).json([{ xsappname: JOBSCH_CREDENTIALS.uaa.xsappname }]);
 });
 
 app.put("/handleSubscription/:myConsumer", (req, res) => {
-   console.log("Handle subscription...");
-   const subDomain = req.body.subscribedSubdomain; // e.g. customer1subdomain
-   const appHost = req.hostname; //schedulermulti.cfapps.sap.hana.ondemand.com
-   const subscriberAppURL = `https://${subDomain}-${appHost}/app`;
-
-   console.log("Subdomain: ", subDomain);
-   console.log("Host: ", appHost);
-   console.log("Subscriber URL: ", subscriberAppURL);
-
-   res.status(200).send(subscriberAppURL);
+   console.log(
+      `==> onSubscription: the TenantID of subscriber: ${req.body.subscribedTenantId}`
+   );
+   const appHost = req.hostname;
+   const subDomain = req.body.subscribedSubdomain;
+   res.status(200).send(`https://${subDomain}-${appHost}/app`);
 });
 
 app.delete("/handleSubscription/:myConsumer", (req, res) => {
    res.status(200).end("unsubscribed");
 });
+
+/* HELPER */
+
+const fetchJWTToken = async (subdomain) => {
+   const uaadomain = JOBSCH_CREDENTIALS.uaa.uaadomain;
+   const oauthEndpoint = `https://${subdomain}.${uaadomain}`;
+   const url = `${oauthEndpoint}/oauth/token?grant_type=client_credentials&response_type=token`;
+
+   console.log("URL to get JWT token: ", url);
+   console.log("JOB CLIENT ID: ", JOBSCH_CLIENTID);
+   console.log("JOB SECRET: ", JOBSCH_SECRET);
+
+   try {
+      let response = await axios.get(url, {
+         headers: {
+            Authorization:
+               "Basic " +
+               Buffer.from(JOBSCH_CLIENTID + ":" + JOBSCH_SECRET).toString(
+                  "base64"
+               ),
+         },
+      });
+
+      return response.data.access_token;
+   } catch (error) {
+      console.log("Error: ", error);
+      return null;
+   }
+};
+
+async function fetchJwtToken(subdomain) {
+   return new Promise((resolve, reject) => {
+      const uaadomain = JOBSCH_CREDENTIALS.uaa.uaadomain;
+      const oauthEndpoint = `${subdomain}.${uaadomain}`;
+      const options = {
+         host: oauthEndpoint,
+         path: "/oauth/token?grant_type=client_credentials&response_type=token",
+         headers: {
+            Authorization:
+               "Basic " +
+               Buffer.from(JOBSCH_CLIENTID + ":" + JOBSCH_SECRET).toString(
+                  "base64"
+               ),
+         },
+      };
+
+      https.get(options, (res) => {
+         res.setEncoding("utf8");
+         let response = "";
+         res.on("data", (chunk) => {
+            response += chunk;
+         });
+
+         res.on("end", () => {
+            try {
+               const responseAsJson = JSON.parse(response);
+               resolve(responseAsJson.access_token);
+            } catch (error) {}
+         });
+      });
+   });
+}
+
+
+const createJob = async (jwtToken, subdomain) => {
+   const url = `${JOBSCH_URL}/scheduler/jobs`;
+   const payload = JSON.stringify({
+      name: `schedulermulti_${new Date().getMilliseconds()}`,
+      action: `https://${subdomain}-schedulermulti.cfapps.eu10-004.hana.ondemand.com/action`,
+      active: true,
+      httpMethod: "GET",
+      schedules: [
+         {
+            time: "now",
+            active: "true",
+         },
+      ],
+   });
+
+   try {
+      await axios.post(url, payload, {
+        headers: {
+          Authorization: "Bearer " + jwtToken,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      return true;
+    } catch (error) {
+      console.log("Error: ", error);
+      return false;
+    }
+}
+
+// async function createJob(jwtToken, subdomain) {
+
+//    return new Promise((resolve, reject) => {
+//       const options = {
+//          host: JOBSCH_URL.replace("https://", ""),
+//          path: `/scheduler/jobs`,
+//          method: "POST",
+//          headers: {
+//             Authorization: "Bearer " + jwtToken,
+//             "Content-type": "application/json",
+//          },
+//       };
+
+//       const req = https.request(options, (res) => {
+//          resolve({
+//             status: `Job result: ${res.statusCode} - ${res.statusMessage}`,
+//          });
+//       });
+
+//       const data = JSON.stringify({
+//          name: `schedulermulti_${new Date().getMilliseconds()}`,
+//          action: `https://${subdomain}-schedulermulti.cfapps.eu10-004.hana.ondemand.com/action`,
+//          active: true,
+//          httpMethod: "GET",
+//          schedules: [
+//             {
+//                time: "now",
+//                active: "true",
+//             },
+//          ],
+//       });
+//       req.write(data);
+//       req.end();
+//    });
+// }
